2de043163d0263df137f7e30c65d038582ecbab9
Spencer <spencer.haka@gmail.com>
2021-01-28 21:05:20 -0700

    First Commit

---
+++ odd_count.js
@@ -0,0 +1,8 @@
+let oddCount = 1;
+
+while (oddCount < 40) {
+  if (oddCount % 2 === 1) {
+    console.log(oddCount);
+  }
+  oddCount += 1;
+}
---
+++ take_two.js
@@ -0,0 +1,10 @@
+/*Write a for loop that iterates over all numbers from 1 to 100, 
+and outputs the result of multiplying each element by 2*/
+
+let count = 1;
+
+while (count <= 100) {
+  count++;
+  console.log(count * 2);
+}
+
